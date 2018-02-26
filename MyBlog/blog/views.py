d07740ba0bfa258ef8601228f7cb3b061ccdf305
Ayanami_Asuka <fyb0118@gmail.com>
2018-02-26 21:57:18 +0800

    细节更新与优化

--- MyBlog/blog/views.py
+++ MyBlog/blog/views.py
@@ -6,7 +6,7 @@ import pygments
 # Create your views here.
 
 def index(request):
-   post_list=Post.objects.all().order_by('-created_time')
+   post_list=Post.objects.all()
    return render(request,'blog/index.html',context={
        'post_list':post_list
        })
